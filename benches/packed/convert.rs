326c48331fe518ce856018adb9287e61f2dbb3e6
Christopher N. Hesse <raymanfx@gmail.com>
2020-07-12 15:34:19 +0200

    benches: Fix convert benchmarks
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- benches/packed/convert.rs
+++ benches/packed/convert.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, Criterion};
 
 use ffimage::color::*;
-use ffimage::core::Convert;
+use ffimage::core::TryConvert;
 use ffimage::packed::{GenericImageBuffer, GenericImageView};
 
 pub fn rgb_to_bgr(c: &mut Criterion) {
@@ -9,14 +9,14 @@ pub fn rgb_to_bgr(c: &mut Criterion) {
     let view = GenericImageView::<Rgb<u8>>::new(&mem, 640, 480).unwrap();
     let mut buf = GenericImageBuffer::<Bgr<u8>>::new(640, 480);
     c.bench_function("RGB[u8] -> BGR[u8] (640x480)", |b| {
-        b.iter(|| view.convert(black_box(&mut buf)))
+        b.iter(|| view.try_convert(black_box(&mut buf)).unwrap())
     });
 
     let mem: Vec<u8> = vec![0; 1280 * 720 * 3];
     let view = GenericImageView::<Rgb<u8>>::new(&mem, 1280, 720).unwrap();
     let mut buf = GenericImageBuffer::<Bgr<u8>>::new(1280, 720);
     c.bench_function("RGB[u8] -> BGR[u8] (1280x720)", |b| {
-        b.iter(|| view.convert(black_box(&mut buf)))
+        b.iter(|| view.try_convert(black_box(&mut buf)).unwrap())
     });
 }
 
@@ -25,14 +25,14 @@ pub fn rgb_to_gray(c: &mut Criterion) {
     let view = GenericImageView::<Rgb<u8>>::new(&mem, 640, 480).unwrap();
     let mut buf = GenericImageBuffer::<Gray<u8>>::new(640, 480);
     c.bench_function("RGB[u8] -> Gray[u8] (640x480)", |b| {
-        b.iter(|| view.convert(black_box(&mut buf)))
+        b.iter(|| view.try_convert(black_box(&mut buf)).unwrap())
     });
 
     let mem: Vec<u8> = vec![0; 1280 * 720 * 3];
     let view = GenericImageView::<Rgb<u8>>::new(&mem, 1280, 720).unwrap();
     let mut buf = GenericImageBuffer::<Gray<u8>>::new(1280, 720);
     c.bench_function("RGB[u8] -> Gray[u8] (1280x720)", |b| {
-        b.iter(|| view.convert(black_box(&mut buf)))
+        b.iter(|| view.try_convert(black_box(&mut buf)).unwrap())
     });
 }
 
