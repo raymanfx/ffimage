32f7e120961d1832f3fd6012d717ef02b038ae3e
Christopher N. Hesse <raymanfx@gmail.com>
2023-02-07 17:08:03 +0100

    yuv: Add more YUV420p utilities
    Handle both packing and unpacking of typed pixel streams and byte chunks.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- ffimage-yuv/src/yuv420.rs
+++ ffimage-yuv/src/yuv420.rs
@@ -8,7 +8,18 @@ use crate::yuv::Yuv;
 pub struct Yuv420p;
 
 impl Yuv420p {
+    /// Returns packed Yuv444 color samples from a given slice.
     pub fn pack<'a, T>(buf: &'a [T]) -> impl IntoIterator<Item = Yuv<T>> + 'a
+    where
+        T: Copy,
+    {
+        Self::pack_bytes(buf)
+            .into_iter()
+            .map(|chunk| Yuv::<T>::from(chunk))
+    }
+
+    /// Returns packed Yuv444 color samples as byte chunks from a given slice.
+    pub fn pack_bytes<'a, T>(buf: &'a [T]) -> impl IntoIterator<Item = [T; 3]> + 'a
     where
         T: Copy,
     {
@@ -27,11 +38,12 @@ impl Yuv420p {
         Yuv420p::pack_planes(y, u, v)
     }
 
+    /// Returns packed Yuv444 color samples as byte chunks from YUV planes.
     pub fn pack_planes<'a, T>(
         y: &'a [T],
         u: &'a [T],
         v: &'a [T],
-    ) -> impl IntoIterator<Item = Yuv<T>> + 'a
+    ) -> impl IntoIterator<Item = [T; 3]> + 'a
     where
         T: Copy,
     {
@@ -39,6 +51,7 @@ impl Yuv420p {
         assert_eq!(y.len() % 4, 0);
         let width = y.len() / 4;
         let height = y.len() / width;
+        assert_eq!(y.len(), width * height);
         assert_eq!(u.len(), width);
         assert_eq!(v.len(), width);
 
@@ -48,10 +61,57 @@ impl Yuv420p {
             .map(move |(i, j)| {
                 let y_idx = i * width + j;
                 let uv_idx = i / 2 * width / 2 + j / 2;
-                let y = y[y_idx];
-                let u = u[uv_idx];
-                let v = v[uv_idx];
-                Yuv([y, u, v])
+                [y[y_idx], u[uv_idx], v[uv_idx]]
             })
+    }
+
+    /// Unpacks packed Yuv444 color samples into Y, U, V planes.
+    ///
+    /// # Arguments
+    ///
+    /// * `yuv444` - Iterator generating Yuv444 samples
+    /// * `y` - Luma plane
+    /// * `u` - Chroma (blue) plane
+    /// * `v` - Chroma (green) plane
+    pub fn unpack<T>(
+        yuv444: impl IntoIterator<Item = Yuv<T>>,
+        y: &mut [T],
+        u: &mut [T],
+        v: &mut [T],
+    ) where
+        T: Copy,
+    {
+        Self::unpack_bytes(yuv444.into_iter().map(|pix| pix.0), y, u, v)
+    }
+
+    /// Unpacks packed Yuv444 color samples into Y, U, V planes.
+    ///
+    /// # Arguments
+    ///
+    /// * `yuv444` - Iterator generating Yuv444 byte chunks
+    /// * `y` - Luma plane
+    /// * `u` - Chroma (blue) plane
+    /// * `v` - Chroma (green) plane
+    pub fn unpack_bytes<T>(
+        yuv444: impl IntoIterator<Item = [T; 3]>,
+        y: &mut [T],
+        u: &mut [T],
+        v: &mut [T],
+    ) where
+        T: Copy,
+    {
+        // YUV420 has 2x2 blocks of Luma (y) samples
+        assert_eq!(y.len() % 4, 0);
+        let width = y.len() / 4;
+        let height = y.len() / width;
+        assert_eq!(y.len(), width * height);
+        assert_eq!(u.len(), width);
+        assert_eq!(v.len(), width);
+
+        yuv444.into_iter().enumerate().for_each(|(i, yuv)| {
+            y[i] = yuv[0];
+            u[i] = yuv[1];
+            v[i] = yuv[2];
+        })
     }
 }
